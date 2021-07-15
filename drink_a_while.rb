9e53fa510ac45cbd5d9a2bc98af1eb81d40c402f
jurijurijuri <jurihorst@gmx.de>
2021-07-15 18:47:53 +0200

    "hey yeaH"

--- drink_a_while.rb
+++ drink_a_while.rb
@@ -1,6 +1,36 @@
-def alk_in_g(prozent, menge)
-	menge/100*prozent*0.8
+class Drink
+		attr_accessor :name
+		attr_accessor :anzahl
+		attr_accessor :alc
+		attr_accessor :vol
+	def drink(name, anzahl, alc, vol)
+		
+	end
+end
+
+class Trinker
+	def trinker
+		@gewicht = gewicht
+		@geschlecht = geschlecht
+	end
+	attr_writer :gewicht
+	attr_writer :geschlecht
+
 end
 
 
+def alk_in_g(alc, vol)
+	vol/100*alc*0.8
+end
+
+def promille (alk_in_g, gewicht, geschlecht)
+	alk_in_g/gewicht*geschlecht
+end
+
+def geschlecht
+
+	
+end
+
 puts alk_in_g(4.7,500)
+
