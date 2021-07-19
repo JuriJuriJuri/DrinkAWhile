f7d975c6885a1f06641f1437037b8f7c74442fee
JuriJuriJuri <87421757+JuriJuriJuri@users.noreply.github.com>
2021-07-19 12:20:48 +0200

    Update DrinkAWhileII.rb

--- DrinkAWhileII.rb
+++ DrinkAWhileII.rb
@@ -4,23 +4,27 @@ end
 
 puts "wie heißt du?"
 username = gets.chomp
-puts "Hallo#{username}, was hast du getrunken?"
+puts "Hallo #{username}, was hast du getrunken?"
 drink_name = gets.chomp
 puts "und wie viele davon?"
-trinker.anzahl = gets.chomp
+anzahl = gets.chomp.to_i
 puts "und wie viel Alk hat der Stoff?"
-drink_alc = gets.chomp
+drink_alc = gets.chomp.to_f
 puts "und wie viel ist da drin?"
-drink_vol = gets.chomp
-vol = drink_anzahl*drink_vol
+drink_vol = gets.chomp.to_i
+vol = anzahl*drink_vol
 puts "und wie viel wiegst du?"
-gewicht = gets.chomp
+gewicht = gets.chomp.to_i
 puts "und bist du ein Mädchen oder ein Junge? (m/j)"
 trinker_geschlecht = gets.chomp.downcase
-	while trinker_geschlecht !="m" || trinker_geschlecht !="j" do puts "junge oder Mädchen?(m/j)"
-	end
-	if  trinker_geschlecht == "j" then geschlecht = 0.7 
-	if trinker_geschlecht == "m" then geschlecht = 0.6 
-	end
-
-puts promille (vol, alc, gewicht, geschlecht)
+ while trinker_geschlecht !="m" && trinker_geschlecht !="j" 
+   puts "junge oder Mädchen?(m/j)"
+   trinker_geschlecht = gets.chomp.downcase
+   if  trinker_geschlecht == "j" then geschlecht = 0.7 
+   end
+   if trinker_geschlecht == "m" then geschlecht = 0.6  
+   end
+  end   
+puts trinker_geschlecht
+puts geschlecht
+  
